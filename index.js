133f9a712419136d4b83d5d6debbc4530d71f246
Camilot12 <jc.toro12@gmail.com>
2023-11-23 22:07:44 -0500

    cambio3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4000;
 
 app.listen(port);
 
-app.get('/', (req,res) => {
-    res.send('Funciona');
+app.get('/', (req, res) => {
+    res.send("Funciona");
 })
 console.log(`Listen on Port ${port}`);
